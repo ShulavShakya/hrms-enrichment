b6d65b638b9734433388c850d4f76979c3c25735
ShulavShakya <shakyashulav29@gmail.com>
2025-07-01 12:23:03 +0545

    optimized the code

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,17 @@
-// // const express = require("express"); //for common js
-// // const mongoose = require("mongoose"); //for common js
-
-// // import express from "express";
-
-// const app = express();
-// const port = 3000;
-
-// app.get("/hello", (req, res) => {
-//   res.send("Hello World");
-// });
-
-// app.listen(port, () => {
-//   console.log(`Example app listening at http://localhost:${port}`);
-// });
-
 import express from "express";
 import mongoose from "mongoose";
-import Employee from "./models/employee.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-const MONGODB_URI =
-  "mongodb+srv://ShulavShakya:dbZenV31x2kRNqYB@hrms.hiq21t9.mongodb.net/?retryWrites=true&w=majority&appName=Hrms";
+const MONGODB_URL = process.env.MONGODB_URI;
 
-const dbConnection = mongoose.connect(MONGODB_URI);
+const dbConnection = mongoose.connect(MONGODB_URL);
 
 dbConnection
   .then(() => {
@@ -37,25 +22,8 @@ dbConnection
     process.exit(1);
   });
 
-app.post("/employee", async (req, res) => {
-  try {
-    const employee = new Employee(req.body);
-    const savedEmployee = await employee.save();
-
-    res.status(201).json({
-      success: true,
-      message: "Employee created succesfully.",
-      data: savedEmployee,
-    });
-  } catch (error) {
-    console.log();
-
-    res.status(500).json({
-      success: false,
-      message: "Error creating employee",
-    });
-  }
-});
+import emp from "./routes/employeeRoutes.js";
+app.use("/api/employees", emp);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);

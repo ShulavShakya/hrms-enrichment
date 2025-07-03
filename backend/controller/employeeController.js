9b489f79101e49e348d5b710c890861f44efa582
ShulavShakya <shakyashulav29@gmail.com>
2025-07-03 21:28:21 +0545

    Added the getEmployees functionality and the shell for updateEmployess functionality

--- backend/controller/employeeController.js
+++ backend/controller/employeeController.js
@@ -1,8 +1,11 @@
 import Employee from "../models/employee.js";
+import bcrypt from "bcryptjs";
 
 const createEmployee = async (req, res) => {
   try {
     const employee = new Employee(req.body);
+    const hashedPassword = await bcrypt.hash(employee.password, 10);
+    employee.password = hashedPassword;
     const savedEmployee = await employee.save();
 
     res.status(201).json({
@@ -20,4 +23,28 @@ const createEmployee = async (req, res) => {
   }
 };
 
-export default createEmployee;
+const getEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.find();
+    res.status(200).json({
+      success: true,
+      message: "Employee retrieved successfully.",
+      data: employees,
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: false,
+      message: "Error getting employees",
+      error: error.message,
+    });
+  }
+};
+
+const updateEmployees = async (req, res) => {
+  try {
+    const employees = await Employee.update();
+    
+  } catch (error) {}
+};
+
+export { createEmployee, getEmployees };

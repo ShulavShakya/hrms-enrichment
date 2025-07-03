94333bd7d10654af96ca2ce5c91408488edf9b35
ShulavShakya <shakyashulav29@gmail.com>
2025-07-03 21:26:23 +0545

    Added getEmployees route

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -1,8 +1,12 @@
 import express from "express";
-import createEmployee from "../controller/employeeController.js";
+import {
+  createEmployee,
+  getEmployees,
+} from "../controller/employeeController.js";
 
 const router = express.Router();
 
 router.post("/", createEmployee);
+router.get("/", getEmployees);
 
 export default router;

8596549e52887af559150b86e692afe12b59c82f
ShulavShakya <shakyashulav29@gmail.com>
2025-07-07 04:59:10 +0545

    Modified the attendance file

--- backend/models/attendanceModel.js
+++ backend/models/attendanceModel.js
@@ -1,24 +1,27 @@
 import { mongoose } from "mongoose";
 
-const attendanceSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
+const attendanceSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    date: {
+      type: Date,
+    },
+    clockIn: {
+      type: Date,
+    },
+    clockOut: {
+      type: Date,
+    },
+    status: {
+      type: String,
+      enum: ["present", "absent", "leave"],
+    },
   },
-  date: {
-    type: Date,
-  },
-  clockIn: {
-    type: Date,
-  },
-  clockOut: {
-    type: Date,
-  },
-  status: {
-    type: String,
-    enum: ["present", "absent", "leave"],
-  },
-});
+  { timestamp: true }
+);
 
 const Attendance = mongoose.model("Attendance", attendanceSchema);
 

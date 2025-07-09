fbedbc16941b7258d2976b024ca6deef9bebdb46
ShulavShakya <shakyashulav29@gmail.com>
2025-07-09 23:55:57 +0545

    Added authentication for login and prototype for for based login

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 import Employee from "../models/employee.js";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const login = async (req, res) => {
   try {
@@ -20,18 +23,16 @@ const login = async (req, res) => {
         message: "Invalid email or password",
       });
     }
-
-    const token = jwt.sign(
-      { id: employee.id, email: employee.email },
-      "my-secret-is-my-secret-none-of-your-secret",
-      { expiresIn: "1hr" }
-    );
+    const key = process.env.KEY;
+    const token = jwt.sign({ id: employee._id, email: employee.email }, key, {
+      expiresIn: "1hr",
+    });
     res.cookie("token", token, {
       httpOnly: true,
       expires: new Date(Date.now() + 3000000),
       secure: process.env.NODE_ENV === "production",
     });
-    res.status(200).json({
+    return res.status(200).json({
       status: true,
       message: "Logged in successfully",
       data: token,

7bc7386cf1466a04f4c737ee11f10749efeaa0c6
ShulavShakya <shakyashulav29@gmail.com>
2025-07-09 23:57:43 +0545

    Added middleware to intercept the data

---
+++ backend/middleware/authMiddleware.js
@@ -0,0 +1,43 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+import Employee from "../models/employee.js";
+
+dotenv.config();
+const authenticateToken = async (req, res, next) => {
+  try {
+    const token = req.cookies.token;
+
+    if (!token) {
+      res.status("401").json({
+        status: false,
+        message: "Unauthorized",
+      });
+    }
+
+    const key = process.env.KEY;
+    const decoded = jwt.verify(token, key);
+
+    req.employee = await Employee.findById(decoded.id).select("-password");
+    next();
+  } catch (Exception) {
+    console.log(Exception.message);
+    res.status(401).json({
+      status: false,
+      message: "Unauthorized",
+    });
+  }
+};
+
+const checkRoles = (...allowedRoles) => {
+  return async (req, res, next) => {
+    if (!allowedRoles.includes(req.employee.role)) {
+      return res.status(403).json({
+        status: false,
+        message: "Access denied",
+      });
+    }
+    next();
+  };
+};
+
+export { authenticateToken, checkRoles };

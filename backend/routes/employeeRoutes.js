1c5984996e273e2dc99f44c08717ff8ab9f753d1
ShulavShakya <shakyashulav29@gmail.com>
2025-07-12 00:32:37 +0545

    Added the middleware in all the endpoints

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -10,11 +10,10 @@ import { authenticateToken, checkRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post("/", [authenticateToken, checkRoles("admin")], createEmployee);
-// router.post("/", createEmployee);
-router.get("/", getEmployees);
-router.get("/:id", getEmployeeById);
-router.delete("/:id", deleteEmployeeById);
-router.put("/:id", updateEmployee);
+router.post("/", [authenticateToken, checkRoles(["admin", "manager"])], createEmployee);
+router.get("/", [authenticateToken], getEmployees);
+router.get("/:id", [authenticateToken], getEmployeeById);
+router.delete("/:id", [authenticateToken, checkRoles(["admin", "manager"])], deleteEmployeeById);
+router.put("/:id", [authenticateToken, checkRoles(["admin", "manager"])], updateEmployee);
 
 export default router;

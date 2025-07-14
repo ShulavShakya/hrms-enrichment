e95cd9dd7d596575e5a2240a29eaa5fcbac49b83
ShulavShakya <shakyashulav29@gmail.com>
2025-07-14 15:53:48 +0545

    added the middlewares

--- backend/routes/employeeRoutes.js
+++ backend/routes/employeeRoutes.js
@@ -6,14 +6,26 @@ import {
   deleteEmployeeById,
   updateEmployee,
 } from "../controller/employeeController.js";
+import { upload } from "../utils/imageUpload.js";
 import { authenticateToken, checkRoles } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
-
-router.post("/", [authenticateToken, checkRoles(["admin", "manager"])], createEmployee);
+router.post(
+  "/",
+  [authenticateToken, checkRoles(["admin", "manager"]), upload.single("image")],
+  createEmployee
+);
 router.get("/", [authenticateToken], getEmployees);
 router.get("/:id", [authenticateToken], getEmployeeById);
-router.delete("/:id", [authenticateToken, checkRoles(["admin", "manager"])], deleteEmployeeById);
-router.put("/:id", [authenticateToken, checkRoles(["admin", "manager"])], updateEmployee);
+router.delete(
+  "/:id",
+  [authenticateToken, checkRoles(["admin", "manager"])],
+  deleteEmployeeById
+);
+router.put(
+  "/:id",
+  [authenticateToken, checkRoles(["admin", "manager"])],
+  updateEmployee
+);
 
 export default router;

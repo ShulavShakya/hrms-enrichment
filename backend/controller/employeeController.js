d74f4fecd6c5ecb3751449ce08db8857fbd80408
ShulavShakya <shakyashulav29@gmail.com>
2025-07-16 10:42:47 +0545

    Added mail functionality

--- backend/controller/employeeController.js
+++ backend/controller/employeeController.js
@@ -1,5 +1,6 @@
 import Employee from "../models/employee.js";
 import bcrypt from "bcryptjs";
+import { sendEmail } from "../utils/sendEmail.js";
 
 export const createEmployee = async (req, res) => {
   try {
@@ -16,10 +17,17 @@ export const createEmployee = async (req, res) => {
     const employee = new Employee({
       ...otherFields,
       password: hashedPassword,
-      profileImage: req.file.filename, // save uploaded image filename
+      profileImage: req.file?.filename, // save uploaded image filename
     });
 
     const savedEmployee = await employee.save();
+
+    sendEmail(
+      employee.email,
+      `Welcome to our company`,
+      `Hello ${employee.name},
+      Welcome to our company. We are glad to have you on board. Your login credentials are as follow: \nEmail: ${employee.email} \nPassword: ${req.body.password}.\n\nThank you,\nAdmin`
+    );
 
     res.status(201).json({
       success: true,

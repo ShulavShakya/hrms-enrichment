6557c4cd89f2e100363e7b01f4df19f2bd31a8fd
ShulavShakya <shakyashulav29@gmail.com>
2025-08-11 09:41:06 +0545

    Added method in cors

--- backend/index.js
+++ backend/index.js
@@ -17,8 +17,9 @@ app.use(cookieParser());
 app.use("/uploads", express.static("uploads"));
 app.use(
   cors({
-    origin: "https://localhost:5173",
+    origin: "http://localhost:5173",
     credentials: true,
+    method: ["GET", "POST", "PUT", "DELETE"],
   })
 );
 

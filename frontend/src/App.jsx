343aecb15b9054c3049685b4c6cd03451f51ca5f
ShulavShakya <shakyashulav29@gmail.com>
2025-08-12 09:17:26 +0545

    Updated the code

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-
 import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import AdminDashboard from "./pages/Admin/AdminDashboard";
 
 function App() {
   return (
-    <>
-      <Login />
-    </>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/admin-dashboard" element={<AdminDashboard />} />
+    </Routes>
   );
 }
 

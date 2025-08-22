70299ec11f73c5bb5a24a809d0d8c206b340b28c
ShulavShakya <shakyashulav29@gmail.com>
2025-08-22 10:21:54 +0545

    Removed hrms folder

--- HrmsMobile/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./App.tsx", "./app/index.tsx/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
